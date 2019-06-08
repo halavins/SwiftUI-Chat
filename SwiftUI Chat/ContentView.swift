bfc931cf6a92d6d97294df05a2d69b756fcfdcac
Nick Halavins <halvini@gmail.com>
2019-06-07 17:47:16 -0700

    Initial Commit

---
+++ SwiftUI Chat/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI Chat
+//
+//  Created by Nick Halavins on 6/7/19.
+//  Copyright © 2019 AntiChat, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
